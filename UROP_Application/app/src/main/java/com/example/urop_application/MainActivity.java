a792d24a73ed81dc8149498dfa3a755d60d606e2
rbanerjee20 <rinibanerjee@pc-204-73.customer.xenia.ch1.lan>
2019-07-08 22:12:37 +0100

    Added code to read and write from local file

--- UROP_Application/app/src/main/java/com/example/urop_application/MainActivity.java
+++ UROP_Application/app/src/main/java/com/example/urop_application/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.urop_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToNextScreen(View view) {
-        Intent intent = new Intent(this, GetIdThread.class);
+        Intent intent = new Intent(this, AdvertisingID.class);
         startActivity(intent);
+    }
+
+    public void isAdBlockerInstalled(View view) {
+        String found = "true";
+        PackageManager pm = getPackageManager();
+        String packageName = "com.hsv.freeadblockerbrowser";
+        try {
+            pm.getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            found = "false";
+        }
+        Toast toast = Toast.makeText(getApplicationContext(), found, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
